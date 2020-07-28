a4f4fc9f8a06e7a59a1a1472e425cb0585293e9a
surungo <surungo@gmail.com>
2020-07-28 11:41:30 -0300

    Aula 1 version 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import Menu from './components/Menu';
+import dados_iniciais from './data/dados_iniciais.json';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
-    <div>
+    <div style={{ background: "#141414"}}>
       <Menu />
+
+      <BannerMain
+        videoTitle={dados_iniciais.categorias[0].videos[0].titulo}
+        url={dados_iniciais.categorias[0].videos[0].url}
+        videoDescription={"Desc: "+dados_iniciais.categorias[0].videos[0].titulo}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dados_iniciais.categorias[0]}
+      />
+
+      <Footer
+
+      />
+
+
+
+
     </div>
   );
 }
